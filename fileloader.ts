ff41ed45bff7c222e9d76b3557f04ac969ddedca
OstriySous <boyinv@yandex.ru>
2022-06-26 20:38:04 +0300

    Initial Setup

---
+++ fileloader.ts
@@ -0,0 +1,4 @@
+import "file:///C:/Users/boyin/WebstormProjects/Aurorium/src/commands/mod.ts#0";
+import "file:///C:/Users/boyin/WebstormProjects/Aurorium/src/commands/ping.ts#0";
+import "file:///C:/Users/boyin/WebstormProjects/Aurorium/src/events/interactionCreate.ts#1";
+import "file:///C:/Users/boyin/WebstormProjects/Aurorium/src/events/ready.ts#1";
