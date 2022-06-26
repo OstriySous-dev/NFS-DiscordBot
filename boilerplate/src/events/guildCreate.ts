03e48e884c21fcea5e5c69218441a9a5218a7216
OstriySous <boyinv@yandex.ru>
2022-06-26 12:36:42 +0300

    Project reload

--- boilerplate/src/events/guildCreate.ts
+++
@@ -1,7 +0,0 @@
-import { botCache } from "../../deps.ts";
-
-botCache.eventHandlers.guildCreate = function (guild) {
-  console.info(
-    `[EVENT=GuildCreate]: ${guild.name} with ${guild.memberCount} members.`,
-  );
-};
